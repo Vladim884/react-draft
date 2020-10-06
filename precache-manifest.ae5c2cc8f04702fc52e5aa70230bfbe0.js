fd66b7bfb09cff4e3d671734e6abdb05790f7d7a
vladim <vladim.volovenko@gmail.com.com>
2020-10-06 23:39:31 +0300

    Updates

---
+++ precache-manifest.ae5c2cc8f04702fc52e5aa70230bfbe0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "154f7c30cb6281b8a549c12b9324000a",
+    "url": "/react-draft/index.html"
+  },
+  {
+    "revision": "6e906dc441b2da0b3073",
+    "url": "/react-draft/static/css/main.1a55f976.chunk.css"
+  },
+  {
+    "revision": "a7814c7f62fb4b80fd6c",
+    "url": "/react-draft/static/js/2.da1f0a2c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-draft/static/js/2.da1f0a2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e906dc441b2da0b3073",
+    "url": "/react-draft/static/js/main.9aa19259.chunk.js"
+  },
+  {
+    "revision": "d239c66c9a340ab755a6",
+    "url": "/react-draft/static/js/runtime-main.c5e6a6b3.js"
+  }
+]);
